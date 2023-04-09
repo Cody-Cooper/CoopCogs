a15478f8791276929d5f85ff93a25aeedddae9c8
Cody Cooper <Cody-Cooper@users.noreply.github.com>
2023-04-09 03:14:34 -0400

    Update qbittchecker.py

--- qbittchecker/qbittchecker.py
+++ qbittchecker/qbittchecker.py
@@ -5,7 +5,7 @@ import socket
 class QbittChecker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.qbittorrent_url = 'http://192.168.1.68:8800' # URL of your qBittorrent client
+        self.qbittorrent_url = 'http://172.17.0.2:8800' # URL of your qBittorrent client
         self.qbittorrent_username = 'admin' # Your qBittorrent username
         self.qbittorrent_password = 'adminadmin' # Your qBittorrent password
 
